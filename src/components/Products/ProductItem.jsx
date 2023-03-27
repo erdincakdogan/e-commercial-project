e47b62c1c1d8b99d68a48a72f4446201d329f965
erdincakdogan <erdinc.akdogan@hotmail.com>
2023-03-28 02:54:46 +0300

    test2

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -35,7 +35,7 @@ const ProductItem = ({ product, products, setProducts }) => {
 
       </Counter>
         <br />
-        <button onClick={clickHandler}>Update</button>
+        <button onClick={clickHandler}>Update Data</button>
         <button onClick={deleteHandler} className="btn-delete">
           Delete
         </button>
