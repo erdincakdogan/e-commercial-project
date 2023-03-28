0d357c5954f68fca9fa2bc489c1d0a5754035351
erdincakdogan <erdinc.akdogan@hotmail.com>
2023-03-28 03:06:25 +0300

    test 3

--- src/components/Products/ProductInfo.jsx
+++ src/components/Products/ProductInfo.jsx
@@ -2,7 +2,7 @@
 const ProductInfo = (props) => {
   return (
     <div className="product-info">
-        <span className="ad">e-shop</span>
+        <span className="ad">e-shop data</span>
         {props.children}
     </div>
   )
